b1320590216ca2f20f34813b0f6156545d2fdf77
tjustino <thomas.justino@gmail.com>
2018-08-31 11:52:45 +0200

    make seeds more complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,46 @@
 # with its default values. The data can then be loaded with the rails db:seed
 # command (or created alongside the database with db:setup).
 
-Store.create(name:    "Default Store",
-             address: "8 Street Life Avenue\n1234 Here&There",
-             phone:   "123456")
+Store.delete_all
+User.delete_all
+Product.delete_all
 
-User.create(email:                 "admin@email.com",
-            password:              "p@ssw0rd",
-            password_confirmation: "p@ssw0rd",
-            is_admin:              true)
+Store.create!(name:    "Default Store",
+              address: "8 Street Life Avenue\n1234 Here&There",
+              phone:   "123456")
+
+User.create!(email:                 "admin@email.com",
+             password:              "p@ssw0rd",
+             password_confirmation: "p@ssw0rd",
+             is_admin:              true)
+
+Product.create!(name: "Orchidée",
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " \
+                  "Curabitur condimentum ipsum nunc, nec varius sem molestie " \
+                  "in. Nulla facilisi. Praesent id neque leo. Interdum et " \
+                  "malesuada fames ac ante ipsum primis in faucibus. Etiam " \
+                  "lacinia enim id orci sagittis, sed viverra dolor dapibus.",
+                image_url: "original_1.jpg",
+                price: 26.00)
+
+Product.create!(name: "Bonsaï",
+                description:
+                  "Vestibulum dapibus nunc eu elit laoreet, sit amet efficit " \
+                  "erat dictum. Cras gravida turpis et lorem bibendum, a " \
+                  "dapibus arcu fringilla. Fusce ut nisl diam. Quisque sit " \
+                  "amet ligula ante. Vivamus ac ultrices elit, in semper " \
+                  "neque. Fusce tempor efficitur eros, nec ultrices neque " \
+                  "pharetra et.",
+                image_url: "original_2.jpg",
+                price: 34.90)
+
+Product.create!(name: "Citronnier",
+                description:
+                  "In a erat vitae lacus venenatis tincidunt id et felis. " \
+                  "Vivamus eros ipsum, congue sed ligula cursus, volutpat " \
+                  "porttitor tellus. Mauris lobortis libero sapien convallis " \
+                  "fringilla. Integer ut nulla rutrum, imperdiet erat non, " \
+                  "bibendum erat.",
+                image_url: "original_3.jpg",
+                price: 55.45)
