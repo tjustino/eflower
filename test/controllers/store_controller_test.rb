92a4899a63b10be33eaebd7db7c9f933d5e9b578
tjustino <thomas.justino@gmail.com>
2018-10-08 16:29:02 +0200

    add some tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,5 +6,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_url
     assert_response :success
+    assert_select   "h2", "Bonsaï"
+    assert_select   "ul.catalog > li", 3
   end
 end
