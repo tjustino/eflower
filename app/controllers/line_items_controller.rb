aa637f0480b7a73dba3c604b39af7b0224694840
tjustino <thomas.justino@gmail.com>
2018-12-05 12:11:35 +0100

    add some ajax features

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,10 +27,13 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
 
-    if @line_item.save
-      redirect_to @line_item.cart
-    else
-      render :new
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
+      else
+        format.html { render :new }
+      end
     end
   end
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,9 @@
 
 # Store Controller
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:name)
   end
